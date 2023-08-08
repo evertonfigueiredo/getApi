1c6b5e9fae348f624e9aafb7428e132149e911ce
Everton Figueiredo <j.everton.figueiredo@gmail.com>
2023-08-07 21:27:53 -0300

    Novo projeto

---
+++ src/App.jsx
@@ -0,0 +1,50 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  const callApi = () => {
+   try {
+
+
+    
+    fetch("https://viacep.com.br/ws/55750000/json/")
+    .then(response => response.json())
+    .then(data => console.log(data))
+
+   } catch (error) {
+    console.error(error);
+   }
+  }
+
+  return (
+    <>
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <button onClick={callApi}>Chamar API</button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
+
+export default App
